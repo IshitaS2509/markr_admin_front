1647a6b435a4f1f0272b6f19406662613f3f21a4
IshitaS2509 <117073024+IshitaS2509@users.noreply.github.com>
2024-11-09 23:23:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminAboutPage from './components/AdminAboutPage/AdminAboutPage';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <AdminAboutPage />
+      <BrowserRouter>
+      <Navbar/>
+      <br/>
+      <Routes>
+        <Route path="/" element= {<Home/>}/>
+        <Route path="/about" element= {<AdminAboutPage/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
